2ac74d93313d7f020e22b6e68af8c14df67a4948
SND\Cheeso_cp <SND\Cheeso_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2010-04-27 16:08:23 +0000

    minor edits
    git-svn-id: https://xpathvisualizer.svn.codeplex.com/svn/XPathVisualizer@42287 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- Tool/XPathVisualizerTool.Designer.cs
+++ Tool/XPathVisualizerTool.Designer.cs
@@ -522,7 +522,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label3.Location = new System.Drawing.Point(33, 158);
+            this.label3.Location = new System.Drawing.Point(18, 131);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(478, 31);
             this.label3.TabIndex = 81;
